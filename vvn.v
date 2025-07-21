7dcddc74d3f93c060e00304bf392873c3548798b
Eliyaan <nopana1007@gmail.com>
2025-07-21 12:50:28 +0200

    fixed compilation errors

--- vvn.v
+++ vvn.v
@@ -5,6 +5,7 @@ import emmathemartian.von
 import os
 
 struct Line {
+mut:
 	chars map[string]string
 	bg    string
 	l     string
@@ -13,6 +14,7 @@ struct Line {
 }
 
 struct Choice {
+mut:
 	l    string
 	next string
 	ifs  map[string]von.Value
@@ -26,12 +28,12 @@ mut:
 	chars         map[string]map[string]gg.Image
 	data          map[string]von.Value
 	dlines        map[string]Line
-	current_dline von.Value
+	current_dline Line
 }
 
 fn run(bg_path string, chars_path string, data_path string, dlines_path string) ! {
 	mut app := &App{
-		data: von.parse_file(data_path)! as Map.values
+		data: (von.parse_file(data_path)! as von.Map).values
 	}
 	app.ctx = gg.new_context( // TODO: out of module
 		create_window: true
@@ -42,61 +44,60 @@ fn run(bg_path string, chars_path string, data_path string, dlines_path string) 
 		sample_count:  2
 	)
 
-	bg_values := von.parse_file(bg_path)! as Map.values
+	bg_values := (von.parse_file(bg_path)! as von.Map).values
 	for name in bg_values.keys() {
-		if bg_values[name] is string {
-			path := bg_values[name] as string
+		path := bg_values[name] or { von.Value('') }
+		if path is string {
 			if os.exists(path) {
-				app.bgs[name] = gg.create_image(path)!
+				app.bgs[name] = app.ctx.create_image(path)!
 			} else {
 				return error('background image ${path} not found')
 			}
 		} else {
-			return error('${bg_values[name]} is not a string path (from the bg name ${name})')
+			return error('${path} is not a string path (from the bg name ${name})')
 		}
 	}
 
-	chars_values := von.parse_file(chars_path)! as Map.values
+	chars_values := (von.parse_file(chars_path)! as von.Map).values
 	for name in chars_values.keys() {
-		if chars_values[name] is string {
-			cname := bg_values[name] as string
-			for iname in chars_values[cname].keys() {
-				if chars_values[cname][iname] is string {
-					ipath := chars_values[cname][imane] as string
+		char_von := chars_values[name] or { von.Value('') }
+		if char_von is von.Map {
+			char_map := unsafe { &char_von.values }
+			for iname in char_map.keys() {
+				ipath := unsafe { char_map[iname] }
+				if ipath is string {
 					if os.exists(ipath) {
-						app.chars[cname][iname] = gg.create_image(ipath)!
+						app.chars[name][iname] = app.ctx.create_image(ipath)!
 					} else {
 						return error('char image ${ipath} not found')
 					}
 				} else {
-					return error('${chars_values[cname][iname]} is not a string path (from the char name ${cname})')
+					return error('${ipath} is not a string path (from the char name ${name})')
 				}
 			}
 		} else {
-			return error('${chars_values[name]} is not a string path (from the char name ${name})')
+			return error('${char_von} is not a string path (from the char name ${name})')
 		}
 	}
 
-	dlines_values := von.parse_file(dlines_path)! as Map.values
+	dlines_values := (von.parse_file(dlines_path)! as von.Map).values
 	for name in dlines_values.keys() {
-		if dlines_values[name] is von.Map {
-			if dlines_values[name] as von.Map.type != 'Line' {
-				return error('${dlines_values[name]} is not a `Line`')
+		line_von_map := dlines_values[name] or { von.Value('') }
+		if line_von_map is von.Map {
+			if line_von_map.type or { '' } != 'Line' {
+				return error('${line_von_map} is not a `Line`')
 			}
-			line_map := dlines_values[name] as von.Map.values
+			line_map := unsafe { &line_von_map.values }
 			mut line := Line{}
-			lchars_values := line_map['chars'] or {
-				return error('Did not find `chars` field for line ${name}')
-			}
+			lchars_values := unsafe { line_map['chars'] or { return error('Did not find `chars` field for line ${name}') } }
 			if lchars_values is von.Map {
-				for cname in lchars_values.keys() {
-					if cname !in app.chars {
-						return error('Unknown char ${cname} in ${app.chars.keys()}')
-					}
-					char_img := lchars_values[cname]
+				lchars := unsafe { &lchars_values.values }
+				for cname in lchars.keys() {
+					char_imgs := unsafe { &app.chars[cname] or { return error('Unknown char ${cname} in ${app.chars.keys()}') }}
+					char_img := unsafe { lchars[cname] }
 					if char_img is string {
-						if char_img !in app.chars[cname] {
-							return error('Unknown charimg ${char_img} for ${cname} in ${app.chars[cname].keys()}')
+						if char_img !in char_imgs {
+							return error('Unknown charimg ${char_img} for ${cname} in ${char_imgs.keys()}')
 						}
 						line.chars[cname] = char_img
 					} else {
@@ -106,24 +107,22 @@ fn run(bg_path string, chars_path string, data_path string, dlines_path string) 
 			} else {
 				return error('${lchars_values} is not a Map (for chars of line `${name}`)')
 			}
-			lbg_value := line_map['bg'] or {
-				return error('Did not find `bg` field for line ${name}')
-			}
+			lbg_value := unsafe { line_map['bg'] or { return error('Did not find `bg` field for line ${name}') }}
 			if lbg_value is string {
 				if lbg_value !in app.bgs {
-					return error('Unknown char ${cname} in ${app.chars.keys()}')
+					return error('Unknown bg ${lbg_value} in ${app.bgs.keys()}')
 				}
 				line.bg = lbg_value
 			} else {
 				return error('${lbg_value} is not a string (for bg of line `${name}`)')
 			}
-			ll_value := line_map['l'] or { return error('Did not find `l` field for line ${name}') }
+			ll_value := unsafe { line_map['l'] or { return error('Did not find `l` field for line ${name}') }}
 			if ll_value is string {
 				line.l = ll_value
 			} else {
 				return error('${ll_value} is not a string (for l of line `${name}`)')
 			}
-			laudio_value := line_map['audio'] or { von.Value('') }
+			laudio_value := unsafe{line_map['audio'] or { von.Value('') }}
 			if laudio_value is string {
 				if laudio_value != '' {
 					line.l = laudio_value
@@ -132,73 +131,70 @@ fn run(bg_path string, chars_path string, data_path string, dlines_path string) 
 			} else {
 				return error('${laudio_value} is not a string (for audio of line `${name}`)')
 			}
-			lact_value := line_map['act'] or { von.Value('') }
+			lact_value := unsafe{line_map['act'] or { von.Value('') }}
 			if lact_value is string {
-				if laudio_value != '' {
+				if lact_value != '' {
 					line.act << Choice{
 						next: lact_value
 					}
 				} else {
 					return error('You need to specify a act for line ${name}, either \'name_next_line\' or []Choice')
 				}
-			} else if lact_value is []Value {
+			} else if lact_value is []von.Value {
 				for v in lact_value {
 					if v is von.Map {
-						if v.type != 'Choice' {
-							return error('${v} is not a `Choice`')
+						vv := unsafe { &v.values }
+						if v.type or { '' } != 'Choice' {
+							return error('${vv} is not a `Choice`')
 						}
 						mut c := Choice{}
-						c_l_value := v['l'] or { von.Value('') }
+						c_l_value := unsafe { vv['l'] or { von.Value('') } }
 						if c_l_value is string {
 							c.l = c_l_value
 						} else {
-							return error('l ${c_l_value} is not a string (for choice ${v} of line `${name}`)')
+							return error('l ${c_l_value} is not a string (for choice ${vv} of line `${name}`)')
 						}
-						c_next_value := v['next'] or { von.Value('') }
+						c_next_value := unsafe { vv['next'] or { von.Value('') } }
 						if c_next_value is string {
 							if c_next_value == '' {
-								return error('You need to specify next dialogue line for choice ${v}')
+								return error('You need to specify next dialogue line for choice ${vv}')
 							}
 							if c_next_value !in dlines_values {
-								return error('dialogue line ${c_next_value} does not exist (from choice ${v})')
+								return error('dialogue line ${c_next_value} does not exist (from choice ${vv})')
 							}
 							c.next = c_next_value
 						} else {
-							return error('next ${c_next_value} is not a string (for choice ${v} of line `${name}`)')
+							return error('next ${c_next_value} is not a string (for choice ${vv} of line `${name}`)')
 						}
-						c_if_value := v['if'] or {
-							von.Map
-							{}
-						}
+						c_if_value := unsafe { vv['if'] or {von.Map{}}}
 						if c_if_value is von.Map {
-							if c_if_value.keys().len > 0 {
-								for field in c_if_value.keys() {
+							c_if := unsafe { &c_if_value.values }
+							if c_if.keys().len > 0 {
+								for field in c_if.keys() {
 									if field !in app.data { // TODO: check type
-										return error('${field} is not in data.von (if of choice ${v} of line `${name}`)')
+										return error('${field} is not in data.von (if of choice ${vv} of line `${name}`)')
 									}
 								}
-								c.if = c_if_value
+								c.ifs = c_if
 								eprintln('ifs are not supported yet')
 							}
 						} else {
-							return error('if ${c_if_value} is not a Map (for choice ${v} of line `${name}`)')
+							return error('if ${c_if_value} is not a Map (for choice ${vv} of line `${name}`)')
 						}
-						c_csq_value := v['csq'] or {
-							von.Map
-							{}
-						}
+						c_csq_value := unsafe { vv['csq'] or {von.Map{}}}
 						if c_csq_value is von.Map {
-							if c_csq_value.keys().len > 0 {
-								for field in c_csq_value.keys() {
+							c_csq := unsafe { &c_csq_value.values }
+							if c_csq.keys().len > 0 {
+								for field in c_csq.keys() {
 									if field !in app.data { // TODO: check type
-										return error('${field} is not in data.von (csq of choice ${v} of line `${name}`)')
+										return error('${field} is not in data.von (csq of choice ${vv} of line `${name}`)')
 									}
 								}
-								c.csq = c_csq_value
+								c.csq = c_csq
 								eprintln('csq are not supported yet')
 							}
 						} else {
-							return error('csq ${c_csq_value} is not a Map (for choice ${v} of line `${name}`)')
+							return error('csq ${c_csq_value} is not a Map (for choice ${vv} of line `${name}`)')
 						}
 						line.act << c
 					} else {
@@ -210,27 +206,16 @@ fn run(bg_path string, chars_path string, data_path string, dlines_path string) 
 			}
 			app.dlines[name] = line
 		} else {
-			return error('${bg_values[name]} is not a Map (from the line `${name}`)')
+			return error('${line_von_map} is not a Map (from the line `${name}`)')
 		}
 	}
 
-	struct Line {
-		chars map[string]string
-		bg    string
-		l     string
-		audio string
-		act   []Choice // actions
-	}
-
-	struct Choice {
-		l    string
-		next string
-		ifs  map[string]von.Value
-		csq  map[string]von.Value
-	}
-
 	first_dline_name := app.data['vvn_first_dialogue'] or { app.dlines.keys()[0] }
-	app.current_dline = app.dlines[first_dline_name]
+	if first_dline_name is string {
+		app.current_dline = app.dlines[first_dline_name]
+	} else {
+		return error('vvn_first_dialogue: ${first_dline_name} is not string ')
+	}
 
 	app.ctx.run()
 }
